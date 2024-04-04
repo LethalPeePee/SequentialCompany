bca87a3d3f37730449ccc822e13fa3a8d14d908a
LethalPeePee <164049531+LethalPeePee@users.noreply.github.com>
2024-04-03 23:15:13 -0400

    Create StartMatchLevelPatch.cs

---
+++ SequentialCompany/Patches/StartMatchLevelPatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace SequentialCompany.Patches
+{
+
+    [HarmonyPatch(typeof(StartMatchLever))]
+    internal class StartMatchLevelPatch
+    {
+
+    }
+}
