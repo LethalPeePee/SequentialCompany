e6ae8f752ad5148a863e195bd6d2a6fac4b3b644
LethalPeePee <164049531+LethalPeePee@users.noreply.github.com>
2024-04-03 22:40:58 -0400

    Add basics

--- SequentialCompany/Plugin.cs
+++ SequentialCompany/Plugin.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
 namespace SequentialCompany
 {
-    public class Plugin
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SequentialCompany : BaseUnityPlugin
     {
+        private const string modGUID = "LethalPeePee.SequentialCompany";
+        private const string modName = "Sequential Company";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static SequentialCompany Instance;
+
+        internal ManualLogSource mls;
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Sequential Company started");
+
+            harmony.PatchAll(typeof(SequentialCompany));
+        }
     }
 }
